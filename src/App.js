b56746181f5a7d477ae7511c7ad4eb0120f4ea9f
FilipeDio <filipe.acd@gmail.com>
2024-11-04 17:48:36 +0000

    Aside Arranjado com o main content

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import Home from './pages/home'
 import Nav from './components/organisms/nav/nav';
 import Aside from './components/organisms/aside/aside';
 
+import { StyledContainer,MainContent } from './components/organisms/Container/Container.styled';
+
 function App() {
   return (
+
     <div> 
     <BrowserRouter>
-    <Nav></Nav>
-    <Aside></Aside>
-
+   
+    <Nav/>
+    <StyledContainer>
+    <Aside/>
+    <MainContent>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-
+      </MainContent>
+      </StyledContainer>
     </BrowserRouter>
 
     </div>
-
+ 
   );
 }
 
---
+++ src/components/organisms/Container/Container.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledContainer =styled.div`
+    display: flex;
+    flex-direction:row;
+
+`
+
+export const MainContent = styled.div`
+    flex:1;
+    padding:1rem;
+`
